18389a2607b34bc9ff99bc2356e84961c16ba970
yasaswinikunam <130962051+yasaswinikunam@users.noreply.github.com>
2023-04-19 11:32:05 +0530

    Update 19-4-2023.py

--- 19-4-2023.py
+++ 19-4-2023.py
@@ -11,3 +11,16 @@ for i in range(1,x+1)
         print(i,end="")
     print()    
   
+Palindrome
+num=int(input("Enter a number:"))
+x=num
+rev=0            
+while num!=0:
+            rem=num%10
+            rev=rev*10+rem
+            num=num//10
+if x == rev:
+    print("Given number is palindrome")
+else:
+    print("Not a palindrome")            
+            
